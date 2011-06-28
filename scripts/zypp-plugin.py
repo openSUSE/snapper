818253f3ba94283c1929791d48701b5059366cf7
Arvin Schnell <aschnell@suse.de>
2011-06-28 11:55:02 +0200

    - added snapper-zypp-plugin subpackage (fate #308626)

---
+++ scripts/zypp-plugin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from subprocess import Popen, PIPE
+from zypp_plugin import Plugin
+
+class MyPlugin(Plugin):
+
+  def PLUGINBEGIN(self, headers, body):
+
+    args = ["snapper", "create", "--type=pre", "--print-number", "--description=zypp"]
+    self.o = Popen(args, stdout=PIPE).communicate()[0].strip()
+
+    self.ack()
+
+  def PLUGINEND(self, headers, body):
+
+    args = ["snapper", "create", "--type=post", "--pre-number=%s" % self.o]
+    Popen(args)
+
+    self.ack()
+    sys.exit(0)
+
+plugin = MyPlugin()
+plugin.main()
