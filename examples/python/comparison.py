baf4786d12921b0983ff881964bd5edd03a2cd2f
Arvin Schnell <aschnell@suse.de>
2012-09-05 10:09:24 +0200

    - updated example

--- examples/python/comparison.py
+++ examples/python/comparison.py
@@ -14,18 +14,8 @@ num_post = 53
 
 snapper.CreateComparison(config_name, num_pre, num_post)
 
-
 files = snapper.GetFiles(config_name, num_pre, num_post)
 
 for file in files:
-    print file[0], file[1], file[2]
-
-
-snapper.SetUndoAll(config_name, num_pre, num_post, True)
-
-
-undo_steps = snapper.GetUndoSteps("root", num_pre, num_post)
-
-for undo_step in undo_steps:
-    print undo_step[0], undo_step[1]
+    print file[0], file[1]
 
