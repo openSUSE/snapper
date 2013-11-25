3d761a096b68a80fb73904604691a3ecc69f3534
Arvin Schnell <aschnell@suse.de>
2013-11-25 11:49:26 +0100

    - allow to configure snapshotting from zypp-plugin depending on package list (fate#316203)

--- scripts/zypp-plugin.py
+++ scripts/zypp-plugin.py
@@ -2,49 +2,167 @@
 
 from os import readlink, getppid
 from os.path import basename
-from sys import stderr
+from fnmatch import fnmatch
+import logging
 from dbus import SystemBus, Interface
+import xml.dom.minidom as minidom
+import xml.parsers.expat as expat
+import json
 from zypp_plugin import Plugin
+
+
+
+class Solvable:
+
+    def __init__(self, name, important):
+        self.name = name
+        self.important = important
+
+    def __repr__(self):
+        return "name:%s important:%s" % (self.name, self.important)
+
+    def match(self, name):
+        return fnmatch(name, self.name)
+
+
+
+class Config:
+
+    def __init__(self):
+        self.solvables = []
+        self.loglevel = None
+        self.load_file("/etc/snapper/zypp-plugin.conf")
+
+
+    def load_file(self, filename):
+        try:
+            self.load_dom(minidom.parse(filename))
+        except IOError:
+            logging.error("failed to open %s" % filename)
+        except expat.ExpatError:
+            logging.error("failed to parse %s" % filename)
+        except:
+            logging.error("failed to load %s" % filename)
+
+
+    def load_dom(self, dom):
+        try:
+            for tmp1 in dom.getElementsByTagName("solvables"):
+                for tmp2 in tmp1.getElementsByTagName("solvable"):
+                    self.solvables.append(Solvable(tmp2.childNodes[0].data,
+                                                   tmp2.getAttribute("important") == "true"))
+        except:
+            pass
+
 
 
 class MyPlugin(Plugin):
 
-
-  def parse_userdata(self, s):
-    ud = {}
-    for kv in s.split(","):
-      k, v = kv.split("=", 1)
-      k = k.strip()
-      if not k:
-        raise ValueError
-      ud[k] = v.strip()
-    return ud
+    def __init__(self):
+        Plugin.__init__(self)
+        self.num1 = self.num2 = None
+        self.description = ""
+        self.cleanup = "number"
+        self.userdata = {}
 
 
-  def PLUGINBEGIN(self, headers, body):
-
-    exe = basename(readlink("/proc/%d/exe" % getppid()))
-
-    self.userdata = {}
-
-    try:
-      self.userdata = self.parse_userdata(headers['userdata'])
-    except KeyError:
-      pass
-    except ValueError:
-      stderr.write("invalid userdata")
-
-    self.num1 = snapper.CreatePreSnapshot("root", "zypp(%s)" % exe, "number", self.userdata)
-
-    self.ack()
+    def parse_userdata(self, s):
+        userdata = {}
+        for kv in s.split(","):
+            k, v = kv.split("=", 1)
+            k = k.strip()
+            if not k:
+                raise ValueError
+            userdata[k] = v.strip()
+        return userdata
 
 
-  def PLUGINEND(self, headers, body):
+    def get_userdata(self, headers):
+        try:
+            return self.parse_userdata(headers['userdata'])
+        except KeyError:
+            pass
+        except ValueError:
+            logging.error("invalid userdata")
+        return {}
 
-    self.num2 = snapper.CreatePostSnapshot("root", self.num1, "", "number", self.userdata)
 
-    self.ack()
+    def get_solvables(self, body):
+        tmp = json.loads(body)
+        tsl = tmp["TransactionStepList"]
+        solvables = set()
+        for ts in tsl:
+            solvables.add(ts["solvable"]["n"])
+        return solvables
 
+
+    def match_solvables(self, names):
+        found = important = False
+        for name in names:
+            for solvable in config.solvables:
+                if solvable.match(name):
+                    found = True
+                    important = important or solvable.important
+                    if found and important:
+                        return True, True
+        return found, important
+
+
+    def PLUGINBEGIN(self, headers, body):
+
+        logging.info("PLUGINBEGIN")
+
+        logging.debug("headers: %s" % headers)
+
+        self.description = "zypp(%s)" % basename(readlink("/proc/%d/exe" % getppid()))
+        self.userdata = self.get_userdata(headers)
+
+        self.ack()
+
+
+    def COMMITBEGIN(self, headers, body):
+
+        logging.info("COMMITBEGIN")
+
+        solvables = self.get_solvables(body)
+        logging.debug("solvables: %s" % solvables)
+
+        found, important = self.match_solvables(solvables)
+        logging.info("found: %s, important: %s" % (found, important))
+
+        if found or important:
+
+            self.userdata["important"] = "yes" if important else "no"
+
+            logging.info("creating pre snapshot")
+            self.num1 = snapper.CreatePreSnapshot("root", self.description, self.cleanup,
+                                                  self.userdata)
+
+        self.ack()
+
+
+    def COMMITEND(self, headers, body):
+
+        logging.info("COMMITEND")
+
+        if self.num1:
+
+            logging.info("creating post snapshot")
+            self.num2 = snapper.CreatePostSnapshot("root", self.num1, "", self.cleanup,
+                                                   self.userdata)
+
+        self.ack()
+
+
+    def PLUGINEND(self, headers, body):
+
+        logging.info("PLUGINEND")
+
+        self.ack()
+
+
+
+config = Config()
 
 bus = SystemBus()
 
