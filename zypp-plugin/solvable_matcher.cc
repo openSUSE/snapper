6e416609e9942989ca12db9ab9d6076927670e27
Martin Vidner <mvidner@suse.cz>
2019-12-17 10:59:27 +0100

    Copyright and license headers (GPL-2.0-only)

--- zypp-plugin/solvable_matcher.cc
+++ zypp-plugin/solvable_matcher.cc
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2019 SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact SUSE LLC.
+ *
+ * To contact SUSE about this file by physical or electronic mail, you may
+ * find current contact information at www.suse.com.
+ */
+
 #include "solvable_matcher.h"
 
 #include <iostream>
