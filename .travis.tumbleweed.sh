d568d0ee1cc3cf6ce6e8bab37147e7dc38f484cb
Martin Vidner <mvidner@suse.cz>
2020-01-10 12:50:02 +0100

    In Travis, build with coverage enabled

--- .travis.tumbleweed.sh
+++ .travis.tumbleweed.sh
@@ -13,7 +13,7 @@ make package
 # Moreover it does not work in a Docker container (it fails when trying to mount
 # /proc and /sys in the chroot).
 cp package/* /usr/src/packages/SOURCES/
-rpmbuild -bb -D "jobs `nproc`" package/*.spec
+rpmbuild -bb --with coverage -D "jobs `nproc`" package/*.spec
 
 # test the %pre/%post scripts by installing/updating/removing the built packages
 # ignore the dependencies to make the test easier, as a smoke test it's good enough
@@ -22,10 +22,16 @@ rpm -iv --force --nodeps /usr/src/packages/RPMS/*/*.rpm
 # smoke test, make sure snapper at least starts
 snapper --version
 
-# run the integration test
-cd /usr/lib*/snapper/testsuite
+# Run the integration test
+# Running it in the source tree ensures that the coverage report finds it
+pushd /usr/src/packages/BUILD/snapper-*/testsuite-real
 ./setup-and-run-all
-cd -
+popd
+
+# Coverage report
+pushd /usr/src/packages/BUILD/snapper-*
+make coverage
+popd
 
 rpm -Uv --force --nodeps /usr/src/packages/RPMS/*/*.rpm
 # get the plain package names and remove all packages at once
