409b4d9352c5cbec19eb3ac082beabb7ba12698c
Arvin Schnell <aschnell@suse.de>
2024-02-27 08:34:23 +0100

    - removed unused function

--- client/utils/text.h
+++ client/utils/text.h
@@ -20,26 +20,6 @@ const char* _(const char* msgid, const char* msgid_plural, unsigned long int n)
 unsigned mbs_width (const std::string & str);
 
 /**
- * Wrap and indent given \a text and write it to the output stream \a out.
- *
- * TODO
- * - delete whitespace at the end of lines
- * - keep one-letter words with the next
- *
- * \param out       output stream to write to
- * \param test      text to wrap
- * \param indent    number of columns by which to indent the whole text
- * \param wrap_width number of columns the text should be wrapped into
- * \param initial   number of columns by which the first line should be indented
- *                  by default, the first line is indented by \a indent
- *
- */
-void mbs_write_wrapped (
-    std::ostream & out,
-    const std::string & text,
-    unsigned indent, unsigned wrap_width, int initial = -1);
-
-/**
  * Returns a substring of a multi-byte character string \a str starting
  * at screen column \a pos and being \a n columns wide, as far as possible
  * according to the multi-column characters found in \a str.
