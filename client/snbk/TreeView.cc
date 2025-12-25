4fd1715c065151523da6077569a078fb2f2d7ccf
Cheng-Ling Lai <jamesljlster@gmail.com>
2025-12-25 17:19:52 +0800

    Added copyright header for client/snbk/TreeView.cc

--- client/snbk/TreeView.cc
+++ client/snbk/TreeView.cc
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) [2024-2025] SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact Novell, Inc.
+ *
+ * To contact Novell about this file by physical or electronic mail, you may
+ * find current contact information at www.novell.com.
+ */
+
 #include <iostream>
 #include <memory>
 #include <queue>
